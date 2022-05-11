72da90522bbf9dfda43fa368d0bf37540ff3d841
AustenVanBrogen <austenvanbrogen@yahoo.com>
2022-05-10 21:03:30 -0500

    Can now cycle through all of the pictures repeatedly with the change picture button

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+let picArray = ['mario.jpg', 'link.png', 'donkeyKong.png'];
+picIndex = 0;
+
+//increments picIndex, then if its passed the end of the picArray, set picIndex to 0.
+//Change the image in index.html to be the new image in the array
 function changePic(){
-    document.getElementById("theImage").src="images/link.png";
+    (++picIndex === picArray.length) ? picIndex = 0 : picIndex = picIndex;
+    curPic = picArray[picIndex];
+    document.getElementById("theImage").src=`images/${curPic}`;
 }
