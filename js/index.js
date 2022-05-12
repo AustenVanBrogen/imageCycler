b29bfb71f195e3a1a74afb3ca2e07e9dcdfa58ac
AustenVanBrogen <austenvanbrogen@yahoo.com>
2022-05-11 22:14:22 -0500

    Made the images cycle through on startup

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
 let picArray = ['mario.png', 'link.png', 'donkeyKong.png'];
-picIndex = 0;
+let picIndex = 0;
+let firstTimeChanging = true;
+let delayInterval = 3000;
+let timer;
+
+let btnPressed = () => {
+    changePic();
+    if(firstTimeChanging)
+    {
+        firstTimeChanging = false;
+        timer = setInterval(changePic, delayInterval);
+    }
+    clearInterval(timer);
+    timer = setInterval(changePic, delayInterval);
+}
 
 //increments picIndex, then if its passed the end of the picArray, set picIndex to 0.
 //Change the image in index.html to be the new image in the array
-function changePic(){
+let changePic = () => {
     (++picIndex === picArray.length) ? picIndex = 0 : picIndex = picIndex;
     curPic = picArray[picIndex];
     document.getElementById("theImage").src=`images/${curPic}`;
+    //let timer = setTimeout(changePic, delayInterval)
 }
+
