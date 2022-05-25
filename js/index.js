491f19dc951b9b9fdb625b9c1b414b38c21940ae
AustenVanBrogen <austenvanbrogen@yahoo.com>
2022-05-25 15:15:22 -0500

    Added a transition effect where the new picture fades in. But it still needs some work

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+//const { del } = require("express/lib/application");
+
 let picArray = ['mario.png', 'link.png', 'donkeyKong.png'];
 let picIndex = 0;
 let firstTimeChanging = true;
@@ -20,22 +22,42 @@ let btnPressed = () => {
 
 //increments picIndex, then if its passed the end of the picArray, set picIndex to 0.
 //Change the image in index.html to be the new image in the array
-let changePic = () => {
+async function  changePic() {
     (++picIndex === picArray.length) ? picIndex = 0 : picIndex = picIndex;
     curPic = picArray[picIndex];
-    document.getElementById("theImage").src=`images/${curPic}`;
+
+    //Change this so that the new picture is the one that fades in
+    await transition();
+    document.getElementsByClassName("firstImg")[0].src=`images/${curPic}`;
+    // document.getElementsByClassName("firstImg")[0].style.opacity -= 0.1;
+    // if(document.getElementsByClassName("firstImg")[0].style.opacity <= 0)
+    // {
+    //     document.getElementsByClassName("firstImg")[0].style.opacity = 1;
+    // }
 }
 
-// let changePos = () => {
-//     let imgStyle = document.getElementById("theImage").style;
+function transition() {
+    return new Promise(function (resolve, reject) 
+    {
+            // del is the value by which opacity is
+            // decreased every interval
+            let del = 0.01;
 
-//     if(imgStyle.left === "-20rem")
-//     {
-//         imgStyle.left = "20rem";
-//     }
-//     else
-//     {
-//         imgStyle.left = "-20rem";
-//     }
-// }
+            let id = setInterval(fade, 10);
+
+        function fade() 
+        {
+            document.getElementsByClassName("firstImg")[0].style.opacity -= del;
+
+            if(document.getElementsByClassName("firstImg")[0].style.opacity <= 0)
+            {
+                //Temporary for testing
+                document.getElementsByClassName("firstImg")[0].style.opacity = 1;
+
+                clearInterval(id);
+                resolve();
+            }
+        }
+    });
+}
 
